b8ce2a15f02a0aa19151470328bbb4d15643ceae
KateGulenkova1 <s244445@std.novsu.ru>
2022-10-25 21:20:12 +0300

    вариант 2 добавлен макисмальный индекс

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ s = file.readlines()
 numbers = [int(x) for x in s]
 print(min(numbers))
 print(max(numbers))
-print(min(numbers)+ max(numbers))
+print(min(numbers)+max(numbers))
+index = numbers.index(max(numbers))
+print('максимальный индекс равен:', index)
